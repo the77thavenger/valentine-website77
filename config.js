fdaa9dee5986b638942191430c40466a19556304
the77thavenger <44283768+the77thavenger@users.noreply.github.com>
2026-02-07 22:29:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -79,7 +79,7 @@ const CONFIG = {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
         musicUrl: "https://res.cloudinary.com/dpi8lr773/video/upload/v1770480771/Ente_Ellam_Ellam_Alle_Video_Song_Meesamadhavan_Dileep_Kavya_Madhavan_zsb5ln.mp3", // Music streaming URL
-        startText: "🎵 Play",        // Button text to start music
+        startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
     }
